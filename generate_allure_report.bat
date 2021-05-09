fc39e4c1a26902dd9eb4def80c5a8987f45136b8
hammad101088 <36362323+hammad101088@users.noreply.github.com>
2021-05-09 18:36:31 +0200

    Delete generate_allure_report.bat

--- generate_allure_report.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set path=C:\Users\HiTech\.m2\repository\allure\allure-2.13.9\bin;F:\software testing carrer\programms\eclipse\plugins\org.eclipse.justj.openjdk.hotspot.jre.full.win32.x86_64_15.0.2.v20210201-0955\jre\bin;%path%
-allure serve allure-results
-pause
-exit
